cfbade6bce05195f21ba8c3b6d82b3a0c0971c6d
Allendale Nato <natoallendale@gmail.com>
2017-06-01 22:46:48 +0800

    Add prompt status in wp-download

--- wp-download.py
+++ wp-download.py
@@ -12,19 +12,34 @@ def download(ver):
 
     # Create wp-files if it doesn't exist
     if not os.path.exists(dir_path):
+        print 'Creating /wp-files directory.'
         os.makedirs(dir_path)
     
     # Download version
-    r = requests.get(url, stream=True)
-    with open('{}\\{}'.format(dir_path, zip_filename), 'wb') as code:
-        code.write(r.content)
-    
+    print 'DOWNLOADING {}...'.format(zip_filename)
+    try:
+        r = requests.get(url, stream=True, timeout=60)
+        with open('{}\\{}'.format(dir_path, zip_filename), 'wb') as code:
+            code.write(r.content)
+    except requests.ConnectionError:
+        print '[ERROR]: Conenction Error'
+        quit()
+    except requests.Timeout:
+        print '[ERROR]: Connection timeout'
+        quit()
+
+    print '[DONE] Download finished'
+
     # Create extract dir.
-    os.makedirs(extract_path)
+    if not os.path.exists(extract_path):
+        print 'Creating {} directory'.format(extract_path)
+        os.makedirs(extract_path)
 
     # Extract zip version
+    print 'EXTRACTING ' + zip_filename
     z = zipfile.ZipFile(StringIO.StringIO(r.content))
     z.extractall(extract_path)
+    print '[DONE] Extract finish'
 
 download('4.7.5')
 
