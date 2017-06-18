63d6130642fcfa16b7f007011711053da55d42a3
Allendale Nato <natoallendale@gmail.com>
2017-06-18 15:24:59 +0800

    Improve and add line numbers in wp_line_diff

--- wp_line_diff.py
+++ wp_line_diff.py
@@ -1,4 +1,22 @@
-import difflib
+import os
+from difflib import Differ
+from pprint import pprint
+
+
+def diff_filter(diff):
+    lineNum = 0
+    diff_list = []
+    for line in diff:
+        # split off the code
+        code = line[:2]
+        # if the  line is in both files or just b, increment the line number.
+        if code in ("  ", "+ "):
+            lineNum += 1
+        # if this line is only in b, print the line number and the text on the line
+        if code == "+ ":
+            diff_list.append('{}: {}'.format(lineNum, line[2:].strip()))
+    
+    return diff_list
 
 def line_diff(fpath1, fpath2):
     """ Returns an array of unified diff between two files """
@@ -6,6 +24,13 @@ def line_diff(fpath1, fpath2):
         file_1 = f1.readlines()
         file_2 = f2.readlines()
     
-    diff = difflib.unified_diff(file_1, file_2)
-    diff_list = [x for x in list(diff)[2:]]
-    return diff_list
+    diff = Differ().compare(file_1, file_2)
+    return diff_filter(diff)
+
+if __name__ == '__main__':
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    wp_files_path = dir_path + '\\sandbox'
+    cron_a = wp_files_path + '\\wp-a\\wp-cron.php'
+    cron_b = wp_files_path + '\\wp-b\\wp-cron.php'
+
+    pprint (line_diff(cron_a, cron_b))
