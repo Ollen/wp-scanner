3a8cbbc232b5cb7d6d4d9b269e2efdcdc6bd7330
Allendale Nato <natoallendale@gmail.com>
2017-07-12 15:04:44 +0800

    Add ftputil module and tests

--- ftp_connector.py
+++ ftp_connector.py
@@ -1,14 +1,42 @@
+import ftputil, hashlib
 from ftplib import FTP
 
+# Login function
 def ftp_login(host, user, password):
     ftp = FTP(host)
     ftp.login(user, password)
     return ftp
 
+# ftputil test
+def ftputil_test():
+    # Connect to FTP (try-catch)
+    try:
+        con = ftputil.FTPHost('localhost', 'admin', 'admin123')
+    except ftputil.error.FTPError as e:
+        print e
+        return
+    else:
+        print 'Successfully connected to the FTP server.'
+    
+    # Change Directory to Wordpress
+    con.chdir('wordpress')
+    print con.listdir('.')
+
+    # Read MD5 Hash of a file
+    md5 = hashlib.md5()
+    with con.open('xmlrpc.php', 'rb') as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            md5.update(chunk)
+            print md5.hexdigest()
+
+    
+
+
+    # Close Directory
+    con.close()
+
 def main():
-    ftp = ftp_login('localhost', 'admin', 'admin123')
-    ftp.cwd('wordpress')
-    ftp.retrlines('LIST')
+    ftputil_test()
 
 if __name__ == '__main__':
     main()
