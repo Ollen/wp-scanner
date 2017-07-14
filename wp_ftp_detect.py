3cdc783347da0fb299789e0adeb01aab6f9e296a
Allendale Nato <natoallendale@gmail.com>
2017-07-14 16:11:07 +0800

    Add initial wp_ftp_detect

---
+++ wp_ftp_detect.py
@@ -0,0 +1,10 @@
+""" Finds the WordPress dir. and detect its version in the FTP server """
+
+def detect_wp():
+    def find_wp_dir():
+        return
+
+    def get_wp_ver():
+        return
+
+    
