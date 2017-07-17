08a565738c1dc4c7e2b563cee5ceca246e0f26fd
Allendale Nato <natoallendale@gmail.com>
2017-07-17 14:57:24 +0800

    Add locate wp version in ftp dir

--- ftp/ftp_wp_detect.py
+++ ftp/ftp_wp_detect.py
@@ -1,10 +1,55 @@
 """ Finds the WordPress dir. and detect its version in the FTP server """
+import re
+from os.path import normpath, join
+from ftp_connector import ftp_connect
 
-def detect_wp():
-    def find_wp_dir():
-        return
 
-    def get_wp_ver():
-        return
+def find_wp_dir():
+    """ Finds the WP file directory in the FTP server """
+    return
+
+def get_wp_ver(con):
+    """ Finds the WP version in the FTP server """
+    filename = 'version.php'
+    max_depth = 2
+
+    print 'LOCATING "version.php" in WP dir...'
+    file_location = None
+    for root, dirs, files in con.walk('.'):
+        cur_depth = root.count(con.sep)
+        if (filename in files) and (cur_depth <= max_depth):
+            file_location = join(normpath(root), normpath(filename))
+            print '[DONE]: WP version located at: {}'.format(file_location)
+
+    if (not file_location):
+        print '[ERROR]: Cannot identify WP version'
+        con.close()
+        quit()
+
+    print 'SCANNING wp version...'
+    with con.open(file_location, 'r') as f:
+        filetext = f.read()
+    
+    version = re.findall("\\$wp_version = '(.+)';", filetext)
+    print '[DONE]: Found WP version {}'.format(version[0])
+    return version[0]
+
+def detect_wp(con):
+    """ Finds the WP dir and version """
+    return get_wp_ver(con)
+    
+
+
+def test():
+    """ Testing Purposes """
+    con = ftp_connect()
+    # Change to the Wordpress Dir.
+    con.chdir('wordpress')
+    version = detect_wp(con)
+    con.close()
+    
+
+if __name__ == '__main__':
+    test()
 
     
