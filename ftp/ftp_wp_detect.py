15574e2ea8acb63c1d169d114d4df4c2a4963cba
Allendale Nato <natoallendale@gmail.com>
2017-07-28 19:42:29 +0800

    Change version serach depth from 2 to 1

--- ftp/ftp_wp_detect.py
+++ ftp/ftp_wp_detect.py
@@ -7,7 +7,7 @@ from ftp_connector import ftp_connect
 def find_wp_dir(con, wp_path):
     """ Finds the WP file directory in the FTP server """
     if wp_path != None:
-        print 'SCANNING {} for wp version...'.format(wp_path)
+        print 'SCANNING {} for WP version...'.format(wp_path)
         if con.path.exists(wp_path):
             con.chdir(wp_path)
             return
@@ -18,9 +18,9 @@ def find_wp_dir(con, wp_path):
 def get_wp_ver(con):
     """ Finds the WP version in the FTP server """
     filename = 'version.php'
-    max_depth = 2
+    max_depth = 1
 
-    print 'LOCATING "version.php" in WP dir...'
+    print 'LOCATING "version.php"...'
     file_location = None
     for root, dirs, files in con.walk('.'):
         cur_depth = root.count(con.sep)
@@ -29,7 +29,7 @@ def get_wp_ver(con):
             print '[DONE]: WP version located at: {}'.format(file_location)
 
     if (not file_location):
-        print '[ERROR]: Cannot identify WP version'
+        print '[ERROR]: Could not find "version.php"'
         con.close()
         quit()
 
