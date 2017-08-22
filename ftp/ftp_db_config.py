5b7c0cfdec53491f891e3ad3806dd5a118015fb4
Allendale Nato <natoallendale@gmail.com>
2017-08-22 18:43:36 +0800

    Add module docstring in ftp_db_config

--- ftp/ftp_db_config.py
+++ ftp/ftp_db_config.py
@@ -1,20 +1,36 @@
-import json, os
+""" Returns the database config data located in 'db_config.json' """
+import json
+import os
 
 def read_config():
+    """ Returns the decoded config data in 'db_config.json'
+    Will return the decoded config file if 'db_config.json' exists and is a valid JSON format.
+    Otherwise, it will return a False.
+    """
+    # Check if file exists
     if not os.path.isfile('db_config.json'):
         return False
 
+    # Check if file is a valid JSON format.
     try:
         with open('db_config.json') as json_data:
             config = json.load(json_data)
     except ValueError:
-        print('[WARN] Error Decoding config.json')
+        print '[WARN] Error Decoding config.json'
         return False
-    
     return config
 
+
 def check_config_keys(config):
-    if ('DB_CONFIG' not in config):
+    """ Checks if the dictionary keys are valid.
+    Returns a True boolean if all the required keys are present,
+    Otherwise returns a False boolean.
+
+    Keywork Arguments:
+    config -- <Dictionary> Decoded JSON config dictionary 
+    """
+
+    if 'DB_CONFIG' not in config:
         return False
 
     db_config = config['DB_CONFIG']
@@ -25,7 +41,8 @@ def check_config_keys(config):
     return True
 
 def create_default_config():
-    print ('Creating default DB config')
+    """ Creats a 'db_config.json' file if it doesn't exist or is an invalid JSON format. """
+    print 'Creating default DB config'
     default_config = {
         "DB_CONFIG": {
             'user': 'user',
@@ -38,11 +55,13 @@ def create_default_config():
     with open('db_config.json', 'w') as jsonfile:
         json_output = json.dumps(default_config , ensure_ascii=False, sort_keys=True ,indent=4, separators=(',', ': '))
         jsonfile.write(json_output)
-    
+
     return default_config
-    
 
 def get_config():
+    """ Main database config module fetcher.
+    Returns the config data in the 'db_config.json' file.
+    """
     config = read_config()
     if config:
         if check_config_keys(config):
@@ -53,7 +72,3 @@ def get_config():
 
     else:
         return create_default_config()
-
-if __name__ == '__main__':
-    get_config()
-
